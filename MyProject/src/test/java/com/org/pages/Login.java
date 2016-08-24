7d9b481435ec0afcb6047016295d981a7e2d1b67
seleniumwdjj@gmail.com <sachin.qa4it@gmail.com>
2016-08-24 18:07:53 +0530

    2nd commit

--- MyProject/src/test/java/com/org/pages/Login.java
+++ MyProject/src/test/java/com/org/pages/Login.java
@@ -13,14 +13,16 @@ public class Login {
 	    private WebDriver driver;		
 		@Test				
 		public void testEasy() {	
+			 driver = new FirefoxDriver();  
 			driver.get("http://www.guru99.com/selenium-tutorial.html");  
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Free Selenium Tutorials")); 		
 		}	
-		@BeforeTest
+		
+	/*	@BeforeTest
 		public void beforeTest() {	
-		    driver = new FirefoxDriver();  
-		}		
+		   
+		}*/		
 		@AfterTest
 		public void afterTest() {
 			driver.quit();			
